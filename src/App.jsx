26e6cd8ff457e9c95c4f18085479557156a09dad
Lucy <lucyneatdesign@yahoo.ca>
2019-01-08 04:03:34 +0000

    Created ChatBarand Message components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
+import Chatbar from './ChatBar.jsx';
+import Message from './Message.jsx';
+// import Messagelist from './MessageList.jsx';
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <body>
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <Message />
+        <Chatbar />
+      </body>
     );
   }
 }
