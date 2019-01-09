cf13370e2e7db6bfd2d280b967ac28c099783d54
Lucy <lucyneatdesign@yahoo.ca>
2019-01-09 16:11:49 +0000

    Excepting new messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,65 @@
 import React, {Component} from 'react';
 import Chatbar from './ChatBar.jsx';
-import Message from './Message.jsx';
-// import Messagelist from './MessageList.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [
+    {
+      id: 1,
+      username: "Bob",
+      content: "Has anyone seen my marbles?",
+    },
+    {
+      id: 2,
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+  ],
+      currentUser: {name: "Bob"},
+
+      idCounter: 3
+    };
+
+    this._newMessage = this._newMessage.bind(this);
+  }
+
+componentDidMount() {
+  console.log("componentDidMount <App />");
+  setTimeout(() => {
+    console.log("Simulating incoming message");
+    // Add a new message to the list of messages in the data store
+    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+    const messages = this.state.messages.concat(newMessage)
+    // Update the state of the app component.
+    // Calling setState will trigger a call to render() in App and all child components.
+    this.setState({messages: messages})
+  }, 3000);
+}
+
   render() {
+    const currentUser = this.state.currentUser;
+    const messagesAll = this.state.messages;
+    const idCount = this.state.idCounter;
     return (
-      <body>
+      <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
-        <Message />
-        <Chatbar />
-      </body>
+        <MessageList messagesAll={messagesAll}/>
+        <Chatbar idCount={idCount} onEnter={this._newMessage} currentUser={currentUser} />
+      </div>
     );
   }
+
+  _newMessage = (value, count) => {
+    this.setState({ messages: [...this.state.messages,
+      { id: count, username: "Bob", content: value }], idCounter: count });
+  };
 }
+
+
 export default App;
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,41 @@
 import React, {Component} from 'react';
 
 class Chatbar extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      textValue: ''
+    };
+
+    this._updateTextValue = this._updateTextValue.bind(this);
+    this._keyPress = this._keyPress.bind(this);
+  }
+
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
+        <input className="chatbar-message"
+               placeholder="Type a message and hit ENTER"
+               onChange={this._updateTextValue}
+               onKeyDown={this._keyPress} />
       </footer>
     );
   }
+
+  _updateTextValue(e) {
+    this.setState({ textValue: e.target.value });
+  }
+
+  _keyPress(e){
+    if(e.keyCode === 13){
+      const counter = this.props.idCount + 1;
+      this.props.onEnter(this.state.textValue, counter);
+      this.setState({ textValue: '' });
+      e.target.value = '';
+    }
+   }
 }
 export default Chatbar;
