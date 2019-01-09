3369405820617981c29a2a7f7b171399d663cae5
Lucy <lucyneatdesign@yahoo.ca>
2019-01-09 22:18:24 +0000

    Now we can change the name of user

--- src/App.jsx
+++ src/App.jsx
@@ -7,57 +7,82 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ],
       currentUser: {name: "Bob"},
-
-      idCounter: 3
+      messages: [] // messages coming from the server will be stored here as they arrive
     };
 
     this._newMessage = this._newMessage.bind(this);
+    this._newName = this._newName.bind(this);
   }
 
 componentDidMount() {
-  console.log("componentDidMount <App />");
-  setTimeout(() => {
-    console.log("Simulating incoming message");
-    // Add a new message to the list of messages in the data store
-    const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-    const messages = this.state.messages.concat(newMessage)
-    // Update the state of the app component.
-    // Calling setState will trigger a call to render() in App and all child components.
-    this.setState({messages: messages})
-  }, 3000);
+
+  this.socket = new WebSocket('ws://localhost:3001');
+
+  this.socket.onopen = () => {
+    console.log('Connected to WebSocket');
+  };
+
+  this.socket.onmessage = payload => {
+    console.log('Got message from server', payload);
+    const json = JSON.parse(payload.data);
+
+    switch (json.type) {
+      case 'sendMessage':
+        this.setState({
+          messages: [...this.state.messages, json]
+        });
+        break;
+      case 'initial-messages':
+        this.setState({ messages: json.messages });
+        break;
+      default:
+    }
+  };
+
+  this.socket.onclose = () => {
+    console.log('Disconnected from the WebSocket');
+  };
+
+  // console.log("componentDidMount <App />");
+  // setTimeout(() => {
+  //   console.log("Simulating incoming message");
+  //   // Add a new message to the list of messages in the data store
+  //   const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+  //   const messages = this.state.messages.concat(newMessage)
+  //   // Update the state of the app component.
+  //   // Calling setState will trigger a call to render() in App and all child components.
+  //   this.setState({messages: messages})
+  // }, 3000);
 }
 
   render() {
     const currentUser = this.state.currentUser;
     const messagesAll = this.state.messages;
-    const idCount = this.state.idCounter;
     return (
       <div>
         <nav className="navbar">
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList messagesAll={messagesAll}/>
-        <Chatbar idCount={idCount} onEnter={this._newMessage} currentUser={currentUser} />
+        <Chatbar newName={this._newName} onEnter={this._newMessage} currentUser={currentUser} />
       </div>
     );
   }
 
-  _newMessage = (value, count) => {
-    this.setState({ messages: [...this.state.messages,
-      { id: count, username: "Bob", content: value }], idCounter: count });
+  _newName = (nameNew) => {
+    this.setState(prevState => ({
+      currentUser: { ...prevState.currentUser, name: nameNew }
+      }))
+  };
+
+  _newMessage = (value) => {
+    const objectToSend = {
+      type: 'sendMessage',
+      content: value,
+      username: this.state.currentUser.name
+    };
+    this.socket.send(JSON.stringify(objectToSend));
   };
 }
 
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -11,16 +11,18 @@ class Chatbar extends Component {
 
     this._updateTextValue = this._updateTextValue.bind(this);
     this._keyPress = this._keyPress.bind(this);
+    this._nameNew = this._nameNew.bind(this);
   }
 
   render() {
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" onKeyDown={this._nameNew} defaultValue={this.props.currentUser.name} placeholder="Your Name (Optional)" />
         <input className="chatbar-message"
                placeholder="Type a message and hit ENTER"
                onChange={this._updateTextValue}
-               onKeyDown={this._keyPress} />
+               onKeyDown={this._keyPress}
+               ref = "msg"/>
       </footer>
     );
   }
@@ -29,13 +31,21 @@ class Chatbar extends Component {
     this.setState({ textValue: e.target.value });
   }
 
+  _nameNew = (e) => {
+    if(e.keyCode === 13){
+      this.props.newName(e.target.value);
+      this.refs["msg"].focus();
+    }
+   }
+
   _keyPress(e){
     if(e.keyCode === 13){
-      const counter = this.props.idCount + 1;
-      this.props.onEnter(this.state.textValue, counter);
+      this.props.onEnter(this.state.textValue);
       this.setState({ textValue: '' });
       e.target.value = '';
     }
    }
+
+
 }
 export default Chatbar;
